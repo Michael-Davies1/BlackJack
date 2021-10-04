043e3577146d7c885caf582544a57358cfe5fa91
MD210515 <michaeldavies924@gmail.com>
2021-10-04 10:43:50 +0100

    a lot and nothing simultaneously

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,20 +10,25 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Random randomNumber = new Random();
         Integer hiddenCards = 0;
-        hiddenCards = hiddenCards + randomNumber.nextInt(11);
-        hiddenCards = hiddenCards + randomNumber.nextInt(11);
+        hiddenCards = hiddenCards + (randomNumber.nextInt(10) + 1);
+        hiddenCards = hiddenCards + (randomNumber.nextInt(10) + 1);
         if (hiddenCards < 16) {
-            hiddenCards = hiddenCards + randomNumber.nextInt(11);
+            hiddenCards = hiddenCards + (randomNumber.nextInt(10 ) + 1);
         }
         Integer playersCards = 0;
-        playersCards = playersCards + randomNumber.nextInt(11);
-        playersCards = playersCards + randomNumber.nextInt(11);
+        playersCards = playersCards + (randomNumber.nextInt(10) + 1);
+        System.out.println("your first card is");
+        System.out.println();
+        System.out.print(playersCards);
+        System.out.println();
+        System.out.println("the total of your cards is");
+        playersCards = playersCards + (randomNumber.nextInt(10) + 1);
         System.out.print(playersCards);
         System.out.println();
         System.out.println("press Y for an extra card or press N for to keep your cards\n");
         String extraCard = input.nextLine();
-        if (extraCard.equals("Y")) {
-            playersCards = playersCards + randomNumber.nextInt(11);
+        if (extraCard.equalsIgnoreCase("Y")) {
+            playersCards = playersCards + (randomNumber.nextInt(10) + 1);
         }
         System.out.print(playersCards);
         System.out.println();
@@ -35,10 +40,10 @@ public class Main {
                     System.out.println("ha sucker you have lost this game of BlackJack");
                 }
             } else {
-                System.out.println("well done you have one this game of BlackJack");
+                System.out.println("well done you have one this game of BlackJack because your opponent Busted");
             }
         } else {
-            System.out.println("too bad youve lost this game of BlackJack");
+            System.out.println("too bad you have busted in this game of BlackJack");
         }
         System.out.print("your cards were");
         System.out.println();
@@ -47,7 +52,6 @@ public class Main {
         System.out.print("the computers cards were");
         System.out.println();
         System.out.print(hiddenCards);
-
 	// write your code here
     }
 }
